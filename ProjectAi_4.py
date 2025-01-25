f09e37dd24d338c2fc935b37c8fa0154e32c7add
sepehr iravani kazar <109028381+sepehriravani@users.noreply.github.com>
2025-01-25 15:45:42 +0330

    Update ProjectAi_4.py

--- ProjectAi_4.py
+++ ProjectAi_4.py
@@ -34,3 +34,22 @@ class Backgammon:
         else:
             self.board[end][0] += 1
             self.board[end][1] = self.player
+    def undo_move(self, start, end):
+        last_move = self.move_history.pop()
+        self.board[start] = last_move[2]
+        self.board[end] = last_move[3]
+
+    def is_bear_off_possible(self):
+        if self.player == "white":
+            return all(pos == self.player or count == 0 for count, pos in self.board[:18])
+        else:
+            return all(pos == self.player or count == 0 for count, pos in self.board[6:])
+
+    def evaluate_board(self):
+        score = 0
+        for i, (count, player) in enumerate(self.board):
+            if player == self.player:
+                score += count * (24 - i if self.player == "white" else i)
+            elif player is not None:
+                score -= count * (24 - i if self.player == "white" else i)
+        return score
